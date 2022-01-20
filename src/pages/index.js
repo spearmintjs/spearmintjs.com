8b5f93c6eb953d0efc6bf3ab0a17ead9c3ffe5ef
Joseph Nagy <nagyjoseph9@gmail.com>
2022-01-19 20:38:45 -0500

    added security testing description

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 import appDemo from '../assets/images/spearmint.gif';
 import accessibilityLens from '../assets/images/accessibilityLens.gif';
 import saveAndGetTestDemo from '../assets/images/saveAndGetTestDemo.gif';
+import snykDemo from '../assets/images/snyk-test-app.gif';
 import todoTest from '../assets/images/todoTest.png';
 import favicon from '../assets/images/favicon.ico';
 import addedfeatures from '../assets/images/testoptions.gif';
@@ -130,7 +131,7 @@ class Homepage extends React.Component {
                   <h2>Test Saving.</h2>
               </header>
               <p>
-              Thanks to the new login feature, developers can now save their test templates. This allows for a quick render of frequently desired test clusters. Simply click the “Save Test” button, name the test template you have created and you’re good to go. To reload the test template click the “Get Test” button and select the test by the name given. This makes recalling tests much easier than having to re-build the tests like in previous versions. 
+              Thanks to the login feature, developers can save their test templates. This allows for a quick render of frequently desired test clusters. Simply click the “Save Test” button, name the test template you have created and you’re good to go. To reload the test template click the “Get Test” button and select the test by the name given. This makes recalling tests much easier than having to re-build the tests like in previous versions. 
               </p>
             </div>
             <div className='col-6'>
@@ -139,226 +140,22 @@ class Homepage extends React.Component {
           </div>
         </section>
 
-        <section id='four' className='main style2 special'>
-          <header className='major'>
-            <h2>Meet the Team!</h2>
-          </header>
-          <div className='flexName'>
-            <ContributorsCard
-              name='Alan Richardson'
-              linkedIn='https://www.linkedin.com/in/arichardson7'
-              github='https://github.com/alanrichardson7'
-            />
-
-            <ContributorsCard
-              name='Alex Park'
-              linkedIn='https://www.linkedin.com/in/apark0720'
-              github='https://www.github.com/apark0720'
-            />
-
-            <ContributorsCard
-              name='Alfred Sta. Iglesia'
-              linkedIn='https://www.linkedin.com/in/alfred-sta-iglesia/'
-              github='https://github.com/astaiglesia'
-            />
-
-            <ContributorsCard
-              name='Annie Shin'
-              linkedIn='https://www.linkedin.com/in/annieshinn/'
-              github='https://github.com/annieshinn'
-            />
-
-            <ContributorsCard
-              name='Ben Kwak'
-              linkedIn='https://www.linkedin.com/in/ben-kwak/'
-              github='https://github.com/bkwak'
-            />
-
-            <ContributorsCard
-              name='Charlie Maloney'
-              linkedIn='https://www.linkedin.com/in/charlie-maloney/'
-              github='https://github.com/charlie-maloney'
-            />
-
-            <ContributorsCard
-              name='Chloe Aribo'
-              linkedIn='https://www.linkedin.com/in/chloe-aribo/'
-              github='https://www.github.com/HeyItsChloe'
-            />
-
-            <ContributorsCard
-              name='Cornelius Phanthanh'
-              linkedIn='https://www.linkedin.com/in/corneliusphanthanh/'
-              github='https://www.github.com/Corneeltron'
-            />
-
-            <ContributorsCard
-              name='Dave Franz'
-              linkedIn='https://www.linkedin.com/in/dave-franz/'
-              github='https://www.github.com/davefranz'
-            />
-            
-            <ContributorsCard
-              name={`Dieu 'Dieunity' Huynh`}
-              linkedIn='https://www.linkedin.com/in/dieu-huynh/'
-              github='https://www.github.com/dieunity'
-            />
-
-            <ContributorsCard 
-             name='Eric Park' 
-             linkedIn='https://www.linkedin.com/in/ericgpark/' 
-             github='https://github.com/ericgpark' 
-            />
-
-            <ContributorsCard
-              name='Evan Berghoff'
-              linkedIn='https://www.linkedin.com/in/evanberghoff/'
-              github='https://github.com/Berghoer'
-            />
-
-            <ContributorsCard
-              name='Gabriel Christo'
-              linkedIn='https://www.linkedin.com/in/gabriel-christo-44364086/'
-              github='https://github.com/bielchristo'
-            />
-
-            <ContributorsCard
-              name='Johnny Lim'
-              linkedIn='https://www.linkedin.com/in/lim-johnny'
-              github='https://www.github.com/johnny-lim'
-            />
-
-            <ContributorsCard
-              name='Joseph Nagy'
-              linkedIn='https://www.linkedin.com/in/josephmnagy'
-              github='https://github.com/Josephnagy'
-            />
-
-            <ContributorsCard
-              name='Julie Beak'
-              linkedIn='https://www.linkedin.com/in/juliebeak'
-              github='https://www.github.com/julicious100'
-            />
-            
-            <ContributorsCard
-              name='Justin Baik'
-              linkedIn='https://www.linkedin.com/in/justin-baik/'
-              github='https://github.com/JIB3377'
-            />
-
-            <ContributorsCard
-              name='Karen Pinilla'
-              linkedIn='https://www.linkedin.com/in/karen-pinilla/'
-              github='https://github.com/karenpinilla'
-            />
-
-            <ContributorsCard
-              name='Linda Wishingrad'
-              linkedIn='https://www.linkedin.com/in/lindawishingrad/'
-              github='https://www.github.com/lcwish'
-            />
-
-            <ContributorsCard
-              name='Luis Lo'
-              linkedIn='https://www.linkedin.com/in/luis-lo/'
-              github='https://github.com/Luis-KM-Lo'
-            />
-
-            <ContributorsCard
-              name= {`Mahmoud 'Mo' Hmaidi`}
-              linkedIn='http://linkedin.com/in/mahmoud-hmaidi-mo'
-              github='https://github.com/mhmaidi789'
-            />
-
-           <ContributorsCard
-            name='Max Bromet'
-            linkedIn='https://www.linkedin.com/in/max-bromet-2607aa211/'
-            github='https://github.com/mbromet'
-            /> 
-
-            <ContributorsCard
-              name='Max Weisenberger'
-              linkedIn='https://www.linkedin.com/in/maxweisen/'
-              github='https://github.com/MaxWeisen'
-            />
-
-            <ContributorsCard
-              name='Mike Coker'
-              linkedIn='https://www.linkedin.com/in/mike-coker/'
-              github='https://github.com/mbcoker'
-            />
-
-            <ContributorsCard
-              name='Natlyn Phomsavanh'
-              linkedIn='https://linkedin.com/in/natlyn-phom'
-              github='https://github.com/natlynp'
-            />
-
-            <ContributorsCard
-              name='Nicolas Pita'
-              linkedIn='https://www.linkedin.com/in/nicolaspita/'
-              github='https://github.com/nicolaspita'
-            />
-
-            <ContributorsCard
-              name='Owen Eldridge'
-              linkedIn='https://www.linkedin.com/in/owen-eldridge'
-              github='https://github.com/oweneldridge'
-            />
-
-            <ContributorsCard
-              name='Rachel Yoo'
-              linkedIn='https://www.linkedin.com/in/rachel-yoo/'
-              github='https://www.github.com/rachethecreator'
-            />
-
-            <ContributorsCard
-              name='Sean Haverstock'
-              linkedIn='https://www.linkedin.com/in/sean-haverstock/'
-              github='https://github.com/Sean-Haverstock'
-            />
-
-            <ContributorsCard
-            name='Sean Yoo'
-            linkedIn='https://www.linkedin.com/in/seanyyoo/'
-            github='https://github.com/seanyyoo'
-            />
-
-            <ContributorsCard
-              name='Sharon Zhu'
-              linkedIn='https://www.linkedin.com/in/sharonzhu/'
-              github='https://github.com/sharon-zhu'
-            />
-
-            <ContributorsCard
-              name='Sieun Jang'
-              linkedIn='https://www.linkedin.com/in/sieunj/'
-              github='https://github.com/sieunjang'
-            />
-
-            <ContributorsCard
-              name='Terence Petersen'
-              linkedIn='https://www.linkedin.com/in/terence-petersen'
-              github='https://github.com/TERR-inss'
-            />
-
-            <ContributorsCard name='Tristen Wastell' linkedIn='' github='' />
-
-            <ContributorsCard
-              name='Tolan Thornton'
-              linkedIn='https://www.linkedin.com/in/tolanthornton/'
-              github='https://github.com/taoantaoan'
-            />
-
-            <ContributorsCard
-            name='Tyler Martinez'
-            linkedIn='https://www.linkedin.com/in/tylerjamesonmartinez/'
-            github='https://github.com/tytyjameson'
-            />
-
+        <section id='six' className='main style2'>
+          <div className='grid-wrapper'>
+            <div className='col-6'>
+              <img id='accessibility-lens' src={snykDemo} alt='Accessibility Lens App Demo' />
+            </div>
+            <div className='col-6'>
+              <header className='major'>
+                <h2>Security Testing</h2>
+              </header>
+              <p>
+                The latest version of Spearmint leverages the Snyk testing library to give developers the ability to test their application for security vulnerabilities including cross-site scripting (XSS), SQL injection and much more. It also checks your application’s dependencies for vulnerabilities to avoid future fixing efforts and save development time.  
+              </p>
+            </div>
           </div>
         </section>
-            
+
       </Layout>
     );
   }
